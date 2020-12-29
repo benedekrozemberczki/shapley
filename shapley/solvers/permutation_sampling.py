ca836128e88d001db797c431cd98343cf28f45c0
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-12-29 18:57:41 +0000

    PERM

--- shapley/solvers/permutation_sampling.py
+++ shapley/solvers/permutation_sampling.py
@@ -1,11 +1,21 @@
+import random
+import numpy as np
 
-class PermutationSampling(object):
+class PermutationSampler(object):
+
+    def __init__(self, permutations: int=1000):
+        self.permutations = permutations
+
+    def solve_games(self, W: np.ndarray, q: float=0.5): -> Phi:
+        Phi = np.zeros(W.shape)
+        indices = [i for i in range(W.shape[1])]
+        for _ in range(self.permutations):
+            random.shuffle(self.indices)
+            W_perm = W[:, indices]
+            cum_sum = np.cumsum(W_perm, axis=1)
+            pivotal = np.argmax(cum_sum>q, axis=1)
+            Phi[np.arange(W.shape[0]), pivotal] = Phi[np.arange(W.shape[0]), pivotal] + 1
+        Phi = Phi/self.permutations
+        return Phi
 
 
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-
-    def solve_game(self):
-        return self.a+self.b
