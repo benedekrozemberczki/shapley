298a94248feeb0995038abd2973811872906d990
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-12-29 19:34:26 +0000

    PERM

--- test/shapley_test.py
+++ test/shapley_test.py
@@ -10,6 +10,20 @@ def test_permutation_sampling():
     solver = PermutationSampler()
 
     W = np.random.uniform(0,1,(100, 10))
-    Phi = solver.solve_game(W, q = 0.5)
+    Phi = solver.solve_game(W, q = 50)
+
+    assert Phi.shape == W.shape
+
+    solver = PermutationSampler(permutations=10)
+
+    W = np.random.uniform(0,1,(100, 100))
+    Phi = solver.solve_game(W, q = 50)
+
+    assert Phi.shape == W.shape
+
+    solver = PermutationSampler(permutations=10000)
+
+    W = np.random.uniform(0,1,(10, 10))
+    Phi = solver.solve_game(W, q = 7)
 
     assert Phi.shape == W.shape
