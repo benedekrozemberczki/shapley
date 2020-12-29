b1da9cd7a7917f219729a65416faaadfa6aa64a9
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-12-29 19:28:14 +0000

    PERM

--- shapley/solution_concept.py
+++ shapley/solution_concept.py
@@ -1,22 +1,21 @@
-
 """Solution Concept base class."""
 
 class SolutionConcept(object):
-    """SolutionConcept base class with constructor and public methods."""
+    """Solution Concept base class with constructor and public methods."""
 
     def __init__(self):
         """Creating a Solution Concept."""
         pass
 
-    def _check_quota(self, q):
+    def _check_quota(self, q: float):
         assert 0.0<=q
 
-    def _verify_result_shape(self, W, Phi):
+    def _verify_result_shape(self, W: np.ndarray, Phi: np.ndarray):
         assert W.shape == Phi.shape
 
-    def _verify_distribution(self, W, Phi):
+    def _verify_distribution(self, W: np.dnarray, Phi: np.ndarray):
         assert 1 == 1
 
-    def _run_sanity_check(self, W, Phi):
+    def _run_sanity_check(self, W: np.ndarray, Phi: np.ndarray):
         self._verify_result_shape(W, Phi)
         self._verify_distribution(W, Phi)
