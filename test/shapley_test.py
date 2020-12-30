5030c1a3c73023341010b31714f15f9d4c401cbc
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-12-30 20:58:58 +0000

    Fun

--- test/shapley_test.py
+++ test/shapley_test.py
@@ -8,7 +8,6 @@ def test_permutation_sampling():
     """
     Testing the Permutation Sampler class.
     """
-
     solver = PermutationSampler()
 
     W = np.random.uniform(0, 1, (100, 100))
@@ -34,6 +33,47 @@ def test_permutation_sampling():
     assert -math.log(1.0/W.shape[1])-entropy > 0
 
     solver = PermutationSampler(permutations=10000)
+
+    W = np.random.uniform(0, 1, (10, 13))
+    solver.solve_game(W, q = 3)
+    Phi = solver.get_solution()
+    Phi_tilde = solver.get_average_shapley()
+    entropy = solver.get_shapley_entropy()
+
+    assert Phi.shape == W.shape
+    assert Phi_tilde.shape == (W.shape[1],)
+    assert -math.log(1.0/W.shape[1])-entropy > 0
+
+def test_multilinear_extension():
+    """
+    Testing the Permutation Sampler class.
+    """
+
+    solver = MultilinearExtension()
+
+    W = np.random.uniform(0, 1, (100, 100))
+    solver.solve_game(W, q = 10)
+    Phi = solver.get_solution()
+    Phi_tilde = solver.get_average_shapley()
+    entropy = solver.get_shapley_entropy()
+
+    assert Phi.shape == W.shape
+    assert Phi_tilde.shape == (W.shape[1],)
+    assert -math.log(1.0/W.shape[1])-entropy > 0
+
+    solver = MultilinearExtension()
+
+    W = np.random.uniform(0, 1, (100, 50))
+    solver.solve_game(W, q = 10)
+    Phi = solver.get_solution()
+    Phi_tilde = solver.get_average_shapley()
+    entropy = solver.get_shapley_entropy()
+
+    assert Phi.shape == W.shape
+    assert Phi_tilde.shape == (W.shape[1],)
+    assert -math.log(1.0/W.shape[1])-entropy > 0
+
+    solver = MultilinearExtension()
 
     W = np.random.uniform(0, 1, (10, 13))
     solver.solve_game(W, q = 3)
