6036ddbeb93618b20b7299e45924b4ac95dbef60
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-12-31 12:01:34 +0000

    EMC

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,10 @@
 import datetime
 import sphinx_rtd_theme
 import doctest
-import shapley
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('../../'))
 
 extensions = [
     'sphinx.ext.autodoc',
