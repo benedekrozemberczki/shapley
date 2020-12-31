6af919e04b48ba52b4277530c56402af844b45a3
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2020-12-31 12:52:30 +0000

    Shapley

---
+++ examples/permutation_sampler_example.py
@@ -0,0 +1,14 @@
+"""Permutation Sampler Example"""
+
+import numpy as np
+from shapley import PermutationSampler
+
+W = np.random.uniform(0, 1, (1, 10))
+W = W/W.sum()
+q = 0.5
+
+solver = PermutationSampler()
+solver.solve_game(W, q)
+shapley_values = solver.get_solution()
+
+print(shapley_values)
