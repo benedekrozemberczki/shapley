04ef5cf738ce2fdb8b0d44fbaba54c13115c1c40
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2021-01-03 20:57:40 +0000

    Shapley

---
+++ examples/exact_marginal_contributions_example.py
@@ -0,0 +1,14 @@
+"""Multilinear Extension Example"""
+
+import numpy as np
+from shapley import ExpectedMarginalContributions
+
+W = np.random.uniform(0, 1, (20, 20))
+W = W/W.sum()
+q = 0.35
+
+solver = ExpectedMarginalContributions()
+solver.solve_game(W, q)
+shapley_values = solver.get_solution()
+
+print(shapley_values)
