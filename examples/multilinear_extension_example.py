96dcb3707ed85ed78a15862428ffb3d52d3d9377
benedek.rozemberczki <benedek.rozemberczki@gmail.com>
2021-01-03 20:53:53 +0000

    Exact

--- examples/multilinear_extension_example.py
+++ examples/multilinear_extension_example.py
@@ -1,13 +1,13 @@
-"""Exact Enumeration Example"""
+"""Multilinear Extension Example"""
 
 import numpy as np
-from shapley import ExactEnumeration
+from shapley import MultilinearExtension
 
 W = np.random.uniform(0, 1, (1, 7))
 W = W/W.sum()
 q = 0.5
 
-solver = ExactEnumeration()
+solver = MultilinearExtension()
 solver.solve_game(W, q)
 shapley_values = solver.get_solution()
 
