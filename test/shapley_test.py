3242c4f357eca57d74e841d92abda65a91a710d1
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-08-17 20:51:39 +0100

    SH

--- test/shapley_test.py
+++ test/shapley_test.py
@@ -15,38 +15,39 @@ def test_permutation_sampling():
     solver = PermutationSampler()
 
     W = np.random.uniform(0, 1, (100, 100))
-    solver.solve_game(W, q = 50)
+    solver.solve_game(W, q=50)
     Phi = solver.get_solution()
     Phi_tilde = solver.get_average_shapley()
     entropy = solver.get_shapley_entropy()
 
     assert Phi.shape == W.shape
     assert Phi_tilde.shape == (W.shape[1],)
-    assert -math.log(1.0/W.shape[1])-entropy > 0
+    assert -math.log(1.0 / W.shape[1]) - entropy > 0
 
     solver = PermutationSampler(permutations=10000)
 
     W = np.random.uniform(0, 1, (100, 50))
-    solver.solve_game(W, q = 20)
+    solver.solve_game(W, q=20)
     Phi = solver.get_solution()
     Phi_tilde = solver.get_average_shapley()
     entropy = solver.get_shapley_entropy()
 
     assert Phi.shape == W.shape
     assert Phi_tilde.shape == (W.shape[1],)
-    assert -math.log(1.0/W.shape[1])-entropy > 0
+    assert -math.log(1.0 / W.shape[1]) - entropy > 0
 
     solver = PermutationSampler(permutations=10000)
 
     W = np.random.uniform(0, 1, (10, 13))
-    solver.solve_game(W, q = 3)
+    solver.solve_game(W, q=3)
     Phi = solver.get_solution()
     Phi_tilde = solver.get_average_shapley()
     entropy = solver.get_shapley_entropy()
 
     assert Phi.shape == W.shape
     assert Phi_tilde.shape == (W.shape[1],)
-    assert -math.log(1.0/W.shape[1])-entropy > 0
+    assert -math.log(1.0 / W.shape[1]) - entropy > 0
+
 
 def test_multilinear_extension():
     """
@@ -56,38 +57,39 @@ def test_multilinear_extension():
     solver = MultilinearExtension()
 
     W = np.random.uniform(0, 1, (100, 97))
-    solver.solve_game(W, q = 0.25)
+    solver.solve_game(W, q=0.25)
     Phi = solver.get_solution()
     Phi_tilde = solver.get_average_shapley()
     entropy = solver.get_shapley_entropy()
 
     assert Phi.shape == W.shape
     assert Phi_tilde.shape == (W.shape[1],)
-    assert -math.log(1.0/W.shape[1])-entropy > 0
+    assert -math.log(1.0 / W.shape[1]) - entropy > 0
 
     solver = MultilinearExtension()
 
     W = np.random.uniform(0, 1, (100, 48))
-    solver.solve_game(W, q = 0.25)
+    solver.solve_game(W, q=0.25)
     Phi = solver.get_solution()
     Phi_tilde = solver.get_average_shapley()
     entropy = solver.get_shapley_entropy()
 
     assert Phi.shape == W.shape
     assert Phi_tilde.shape == (W.shape[1],)
-    assert -math.log(1.0/W.shape[1])-entropy > 0
+    assert -math.log(1.0 / W.shape[1]) - entropy > 0
 
     solver = MultilinearExtension()
 
     W = np.random.uniform(0, 1, (10, 13))
-    solver.solve_game(W, q = 0.13)
+    solver.solve_game(W, q=0.13)
     Phi = solver.get_solution()
     Phi_tilde = solver.get_average_shapley()
     entropy = solver.get_shapley_entropy()
 
     assert Phi.shape == W.shape
     assert Phi_tilde.shape == (W.shape[1],)
-    assert -math.log(1.0/W.shape[1])-entropy > 0
+    assert -math.log(1.0 / W.shape[1]) - entropy > 0
+
 
 def test_exact_enumeration():
     """
@@ -97,38 +99,38 @@ def test_exact_enumeration():
     solver = ExactEnumeration()
 
     W = np.random.uniform(0, 1, (100, 7))
-    solver.solve_game(W, q = 2.5)
+    solver.solve_game(W, q=2.5)
     Phi = solver.get_solution()
     Phi_tilde = solver.get_average_shapley()
     entropy = solver.get_shapley_entropy()
 
     assert Phi.shape == W.shape
     assert Phi_tilde.shape == (W.shape[1],)
-    assert -math.log(1.0/W.shape[1])-entropy > -0.001
+    assert -math.log(1.0 / W.shape[1]) - entropy > -0.001
 
     solver = ExactEnumeration()
 
     W = np.random.uniform(0, 1, (100, 6))
-    solver.solve_game(W, q = 2)
+    solver.solve_game(W, q=2)
     Phi = solver.get_solution()
     Phi_tilde = solver.get_average_shapley()
     entropy = solver.get_shapley_entropy()
 
     assert Phi.shape == W.shape
     assert Phi_tilde.shape == (W.shape[1],)
-    assert -math.log(1.0/W.shape[1])-entropy > -0.001
+    assert -math.log(1.0 / W.shape[1]) - entropy > -0.001
 
     solver = ExactEnumeration()
 
     W = np.random.uniform(0, 1, (10, 5))
-    solver.solve_game(W, q = 3)
+    solver.solve_game(W, q=3)
     Phi = solver.get_solution()
     Phi_tilde = solver.get_average_shapley()
     entropy = solver.get_shapley_entropy()
 
     assert Phi.shape == W.shape
     assert Phi_tilde.shape == (W.shape[1],)
-    assert -math.log(1.0/W.shape[1])-entropy > -0.001
+    assert -math.log(1.0 / W.shape[1]) - entropy > -0.001
 
 
 def test_expected_marginal_contributions():
@@ -139,35 +141,35 @@ def test_expected_marginal_contributions():
     solver = ExpectedMarginalContributions()
 
     W = np.random.uniform(0, 1, (100, 97))
-    solver.solve_game(W, q = 15.5)
+    solver.solve_game(W, q=15.5)
     Phi = solver.get_solution()
     Phi_tilde = solver.get_average_shapley()
     entropy = solver.get_shapley_entropy()
 
     assert Phi.shape == W.shape
     assert Phi_tilde.shape == (W.shape[1],)
-    assert -math.log(1.0/W.shape[1])-entropy > 0
+    assert -math.log(1.0 / W.shape[1]) - entropy > 0
 
     solver = ExpectedMarginalContributions()
 
     W = np.random.uniform(0, 1, (100, 48))
-    solver.solve_game(W, q = 12.5)
+    solver.solve_game(W, q=12.5)
     Phi = solver.get_solution()
     Phi_tilde = solver.get_average_shapley()
     entropy = solver.get_shapley_entropy()
 
     assert Phi.shape == W.shape
     assert Phi_tilde.shape == (W.shape[1],)
-    assert -math.log(1.0/W.shape[1])-entropy > 0
+    assert -math.log(1.0 / W.shape[1]) - entropy > 0
 
-    solver = ExpectedMarginalContributions(epsilon=10**-5)
+    solver = ExpectedMarginalContributions(epsilon=10 ** -5)
 
     W = np.random.uniform(0, 1, (10, 13))
-    solver.solve_game(W, q = 3.0)
+    solver.solve_game(W, q=3.0)
     Phi = solver.get_solution()
     Phi_tilde = solver.get_average_shapley()
     entropy = solver.get_shapley_entropy()
 
     assert Phi.shape == W.shape
     assert Phi_tilde.shape == (W.shape[1],)
-    assert -math.log(1.0/W.shape[1])-entropy > 0
+    assert -math.log(1.0 / W.shape[1]) - entropy > 0
