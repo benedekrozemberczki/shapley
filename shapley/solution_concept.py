f151e8bb7a1cefb6b73d988fa2df3b67d63f7359
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-10-05 23:29:18 +0100

    Sjapley

--- shapley/solution_concept.py
+++ shapley/solution_concept.py
@@ -20,7 +20,7 @@ class SolutionConcept(object):
 
     def _verify_distribution(self, Phi: np.ndarray):
         """Verify distribution hypothesis."""
-        assert np.sum(Phi) - Phi.shape[0] < 0.001
+        assert (np.sum(Phi) - Phi.shape[0]) < 0.0001
 
     def _run_sanity_check(self, W: np.ndarray, Phi: np.ndarray):
         """Checking the basic assumptions about the Shapley values."""
